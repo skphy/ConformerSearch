d8139d7d7a4a732f195b1c4705766ec301f6a721
Sonu Kumar <1009ukumar@gmail.com>
2021-12-23 15:40:30 +0900

    Create manipulations.py

---
+++ manipulations.py
@@ -0,0 +1,24 @@
+# taken from the aspertinker on github
+
+Molecular Modeling Tools for Tinker
+
+## Read/write chemical structure files
+
+Supported formats: xyz, txyz
+
+* Compute trajectories RMSDs
+* Analyze structures
+  * Distance
+  * Angle
+  * Torsion
+  * Pseudo-rotation angle
+  * Distance to line
+  * Distance to plane
+  * Angle between two planes
+
+## Manipulate chemical structures
+
+* Automatically fragment structures
+* Find principle axes of the interface between two fragments
+* Translate and/or rotate one fragment relative to the other fragment
+* Delete or add trajectories or atoms
